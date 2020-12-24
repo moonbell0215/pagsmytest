40f586b3b9bdfae5d5bfa92b09f72ae4cdc3c0ea
pclev <pclevinjava@gmail.com>
2020-12-24 17:19:23 +0800

    更新domain

---
+++ wallet-domain/src/main/java/com/dht/pags/wallet/domain/BalanceUpdatedEvent.java
@@ -0,0 +1,67 @@
+package com.dht.pags.wallet.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public class BalanceUpdatedEvent {
+    private final String id;
+    private final double transactionAmount;
+    private final String walletId;
+    private final Date transactionDateTime;
+    private final TransactionType transactionType;
+    private final String description;
+
+    @JsonCreator
+    public BalanceUpdatedEvent(@JsonProperty("id") String id,
+                               @JsonProperty("transactionAmount") double transactionAmount,
+                               @JsonProperty("walletId") String walletId,
+                               @JsonProperty("transactionDateTime") Date transactionDateTime,
+                               @JsonProperty("transactionType") TransactionType transactionType,
+                               @JsonProperty("description") String description) {
+        this.id = id;
+        this.transactionAmount = transactionAmount;
+        this.walletId = walletId;
+        this.transactionDateTime = transactionDateTime;
+        this.transactionType = transactionType;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
+
+    public Date getTransactionDateTime() {
+        return transactionDateTime;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("BalanceUpdatedEvent{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", transactionAmount=").append(transactionAmount);
+        sb.append(", walletId='").append(walletId).append('\'');
+        sb.append(", transactionDateTime=").append(transactionDateTime);
+        sb.append(", transactionType=").append(transactionType);
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
