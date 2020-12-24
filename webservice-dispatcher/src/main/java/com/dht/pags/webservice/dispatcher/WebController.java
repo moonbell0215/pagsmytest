b493ffe17ac0f27ca8560644ef0a78f05e66b8e6
ansonau <ansonau@gmail.com>
2020-12-24 12:47:18 +0800

    POC

--- webservice-dispatcher/src/main/java/com/dht/pags/webservice/dispatcher/WebController.java
+++ webservice-dispatcher/src/main/java/com/dht/pags/webservice/dispatcher/WebController.java
@@ -2,20 +2,22 @@ package com.dht.pags.webservice.dispatcher;
 
 import com.dht.pags.wallet.domain.CreateTransactionCommand;
 import com.dht.pags.wallet.domain.TransactionCreatedEvent;
+import com.dht.pags.wallet.domain.TransactionType;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
@@ -25,49 +27,65 @@ import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderOptions;
 import reactor.kafka.sender.SenderRecord;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.function.Predicate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-@Service
 @RestController
 public class WebController {
+    private static Logger LOGGER = LoggerFactory.getLogger(WebController.class);
 
-    private KafkaSender<String, String> kafkaProducer;
+    private KafkaSender<String, CreateTransactionCommand> kafkaProducer;
     private ObjectMapper objectMapper;
-    private Flux<ReceiverRecord<String, TransactionCreatedEvent>> transactionCreatedEventFlux;
+    private Flux<ReceiverRecord<String, String>> transactionCreatedEventFlux;
+    private DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SSS z dd MMM yyyy");
+    private AtomicInteger sendCounter = new AtomicInteger();
 
     public WebController() {
         final Map<String, Object> producerProps = new HashMap<>();
         producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        final SenderOptions<String, String> producerOptions = SenderOptions.create(producerProps);
+        final SenderOptions<String, CreateTransactionCommand> producerOptions = SenderOptions.create(producerProps);
         kafkaProducer = KafkaSender.create(producerOptions);
 
-//        final Map<String, TransactionCreatedEvent> consumerProps = new HashMap<>();
-//        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-//        consumerProps.put(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
-//        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "webservice-dispatcher");
-//        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        ReceiverOptions<String, TransactionCreatedEvent> consumerOptions = ReceiverOptions.create(consumerProps)
-//                .subscription(Collections.singleton("wallet.transactionCreatedEvent"));
-//        transactionCreatedEventFlux = KafkaReceiver.create(consumerOptions).receive();
-
+        final Map<String, Object> consumerProps = new HashMap<>();
+        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerProps.put(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "webservice-dispatcher");
+        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        ReceiverOptions<String, String> consumerOptions = ReceiverOptions.create(consumerProps);
+        consumerOptions.subscription(Collections.singleton("wallet.transactionCreatedEvent"));
+        transactionCreatedEventFlux = KafkaReceiver.create(consumerOptions).receive();
+        transactionCreatedEventFlux.log();
         objectMapper = new ObjectMapper();
     }
 
-//    @PostMapping(value = "/transaction")
-//    public Mono<TransactionCreatedEvent> doTransaction(@RequestBody CreateTransactionCommand command) throws JsonProcessingException {
-//        final String transactionIdFromCommand = command.getTransactionId();
-//        SenderRecord<String, String, Integer> message = SenderRecord.create
-//                (new ProducerRecord<>("wallet.createTransactionCommand", objectMapper.writeValueAsString(command)), 1);
-//        kafkaProducer.send(Mono.just(message));
-//        Mono<ReceiverRecord<String, TransactionCreatedEvent>> mono = transactionCreatedEventFlux.filter(x -> x.value().getId().equals(transactionIdFromCommand)).next();
-//        return mono;
-//    }
+    @PostMapping(value = "/wallets/transaction")
+    public Mono<TransactionCreatedEvent> doTransaction(@RequestBody CreateTransactionCommand command) throws JsonProcessingException {
+        LOGGER.info("/transaction received: " + command.toString());
 
+        final String transactionIdFromCommand = command.getTransactionId();
+        SenderRecord<String, CreateTransactionCommand, Integer> message = SenderRecord.create(new ProducerRecord<>
+                        ("wallet.createTransactionCommand",
+                        transactionIdFromCommand,
+                        command),1);
+
+        kafkaProducer.send(Mono.just(message))
+                .doOnError(e -> LOGGER.error("Send failed", e))
+                .subscribe(r -> {
+            RecordMetadata metadata = r.recordMetadata();
+                    LOGGER.info("Message %d sent successfully, topic-partition=%s-%d offset=%d timestamp=%s\n",
+                    r.correlationMetadata(),
+                    metadata.topic(),
+                    metadata.partition(),
+                    metadata.offset(),
+                    dateFormat.format(new Date(metadata.timestamp())));
+        });
+
+        return Mono.just(new TransactionCreatedEvent(command.getTransactionId(),
+                command.getTransactionAmount(),command.getWalletId(),new Date(), TransactionType.DEPOSIT,command.getDescription()));
+    }
 }
