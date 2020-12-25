412e06c722d124edc51bb45dc3fbc3a1368c4e08
ansonau <ansonau@gmail.com>
2020-12-25 22:36:59 +0800

    Added State Store

--- wallet-domain/src/main/java/com/dht/pags/wallet/domain/TransactionType.java
+++ wallet-domain/src/main/java/com/dht/pags/wallet/domain/TransactionType.java
@@ -1,46 +1,55 @@
 package com.dht.pags.wallet.domain;
 
 public enum TransactionType {
+
     /**
      * 手动冲正，需要设定流水倍数
      * 加钱
      */
-    INCREASE,
+    INCREASE(false),
     /**
      * 手动冲负，需要设定流水倍数
      * 扣钱
      */
-    DECREASE,
+    DECREASE(true),
     /**
      * 存款，默认1倍流水
      * 加钱
      */
-    DEPOSIT,
+    DEPOSIT(false),
     /**
      * 取款申请
      * 扣钱
      */
-    WITHDRAW_APPLY,
+    WITHDRAW_APPLY(true),
     /**
      * 取款拒绝
      * 加钱
      */
-    WITHDRAW_REJECT,
+    WITHDRAW_REJECT(false),
     /**
      * 游戏下注
      * 扣钱
      */
-    BET_PLACE,
+    BET_PLACE(true),
     /**
      * 取消下注
      * 加钱
      */
-    BET_CANCLE,
+    BET_CANCLE(false),
     /**
      * 派彩
      */
-    PAYOUT,
-
-
+    PAYOUT(true),
     ;
+
+    private final boolean isReduce;
+
+    TransactionType(boolean isReduce){
+        this.isReduce = isReduce;
+    }
+
+    public boolean isReduce() {
+        return isReduce;
+    }
 }
